452f351f3a692dd6db641596634fc9f04999d37f
Alexander <deceptimator@yandex.ru>
2021-09-28 15:05:31 +0300

    Modified CHSH.h functions

--- CHSH_calculator.h
+++ CHSH_calculator.h
@@ -6,7 +6,7 @@ namespace CHSH
 
 
 
-    void count_coincidences(Int_t n, Int_t NumEvents[16][16], Int_t &N_parallel, Int_t &N_perpendicular, Int_t &N_mixed_1, Int_t &N_mixed_2)
+    void count_coincidences(Int_t n, Int_t NumEvents[16][16], Int_t &N_parallel, Int_t &N_perpendicular, Int_t &N_mixed_1, Int_t &N_mixed_2, TString z = "clockwise")
     {
             N_parallel=0;
             N_perpendicular = 0;
@@ -15,9 +15,7 @@ namespace CHSH
 
         for (Int_t a = 0; a < 16; a++)
         {
-            //if (string == "clockwise") 
-            Int_t b = a+n;
-            //if (string == "counterclockwise") Int_t b = a-n+16;
+            Int_t b = a+n+16;
             Int_t a_1 = a+4;
             Int_t b_1 = b+4;
             while (a > 15) a=a-16;
@@ -32,92 +30,96 @@ namespace CHSH
         }
     }
 
-    Float_t global_E_coeff(Int_t n, Int_t NumEvents[16][16])
+    Float_t global_E_coeff(Int_t n, Int_t NumEvents[16][16], TString z = "clockwise")
     {
         Int_t N_parallel = 0; Int_t N_perpendicular = 0;Int_t N_mixed_1 = 0;Int_t N_mixed_2 = 0;
         count_coincidences(n,NumEvents, N_parallel, N_perpendicular, N_mixed_1, N_mixed_2);
         return (float)(N_parallel+N_perpendicular-N_mixed_1-N_mixed_2)/(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2)
         ;
     }
-    Float_t global_sqr_E_error(Int_t n,Int_t NumEvents[16][16]) //calculating square error of correlation coefficient
+    Float_t global_sqr_E_error(Int_t n,Int_t NumEvents[16][16], TString z = "clockwise") //calculating square error of correlation coefficient
     {
         Int_t N_parallel = 0; Int_t N_perpendicular = 0;Int_t N_mixed_1 = 0;Int_t N_mixed_2 = 0;
 
         count_coincidences(n,NumEvents, N_parallel, N_perpendicular, N_mixed_1, N_mixed_2);
-        //return pow(*(N_mixed_1+N_mixed_2)/pow((N_parallel+N_perpendicular+N_mixed_1+N_mix)ed_2),2),2)*(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2);
         return (float)4.0/pow((N_parallel+N_perpendicular+N_mixed_1+N_mixed_2),4)*(pow(N_mixed_1+N_mixed_2,2)*(N_parallel+N_perpendicular)+pow(N_parallel+N_perpendicular,2)*(N_mixed_1+N_mixed_2));
     }
-    Float_t global_calculate_CHSH(Int_t n,Int_t NumEvents[16][16])
+    Float_t global_calculate_CHSH(Int_t n,Int_t NumEvents[16][16], TString z = "clockwise")
     {
-
+        if (z == "counterclockwise") n = -n;
         return
         3*global_E_coeff(n, NumEvents) - global_E_coeff(3*n,NumEvents)
         ;
     }
 
-    Float_t global_calculate_CHSH_error(Int_t n,Int_t NumEvents[16][16])
+    Float_t global_calculate_CHSH_error(Int_t n,Int_t NumEvents[16][16], TString z = "clockwise")
     {
+        if (z == "counterclockwise") n = -n;
+
         return
         global_sqr_E_error(n, NumEvents)*3 + global_sqr_E_error(3*n, NumEvents)
         ;
     }
 
-
-
-    Float_t E_coeff(Int_t NumEvents[16][16], Int_t a, Int_t b)
+/////////////////////////////////////////
+/////////////////////////////////////////
+    void local_count_coincidences(Int_t a, Int_t b, Int_t NumEvents[16][16], Int_t &N_parallel, Int_t &N_perpendicular, Int_t &N_mixed_1, Int_t &N_mixed_2, TString z = "clockwise")
     {
+        a += 48;
+        b += 48;
         Int_t a_1 = a+4;
         Int_t b_1 = b+4;
         while (a > 15) a=a-16;
         while (b > 15) b=b-16;
-
         while (a_1> 15) a_1=a_1-16;
         while (b_1 > 15) b_1=b_1-16;    
-        Int_t N_parallel=NumEvents[a][b];
-        Int_t N_perpendicular = NumEvents[a_1][b_1];
-        Int_t N_mixed_1 = NumEvents[a][b_1];
-        Int_t N_mixed_2 = NumEvents[a_1][b];
+        N_parallel=NumEvents[a][b];
+        N_perpendicular = NumEvents[a_1][b_1];
+        N_mixed_1 = NumEvents[a][b_1];
+        N_mixed_2 = NumEvents[a_1][b];
+    }
+
+    Float_t E_coeff(Int_t NumEvents[16][16], Int_t a, Int_t b, TString z = "clockwise")
+    {
+        Int_t N_parallel = 0;
+        Int_t N_perpendicular = 0;
+        Int_t N_mixed_1 = 0;
+        Int_t N_mixed_2 = 0;
+        local_count_coincidences(a,b,NumEvents, N_parallel, N_perpendicular, N_mixed_1, N_mixed_2);
         return (float)(N_parallel+N_perpendicular-N_mixed_1-N_mixed_2)/(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2)
         ;
     }
-    Float_t sqr_E_error(Int_t NumEvents[16][16], Int_t a, Int_t b) //calculating square error of correlation coefficient
-    {
-        Int_t a_1 = a+4;
-        Int_t b_1 = b+4;
-        while (a > 15) a=a-16;
-        while (b > 15) b=b-16;
 
-        while (a_1> 15) a_1=a_1-16;
-        while (b_1 > 15) b_1=b_1-16;    
-        Int_t N_parallel=NumEvents[a][b];
-        Int_t N_perpendicular = NumEvents[a_1][b_1];
-        Int_t N_mixed_1 = NumEvents[a][b_1];
-        Int_t N_mixed_2 = NumEvents[a_1][b];
-        //return pow(2*(N_mixed_1+N_mixed_2)/pow((N_parallel+N_perpendicular+N_mixed_1+N_mix)ed_2),2),2)*(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2);
+    Float_t sqr_E_error(Int_t NumEvents[16][16], Int_t a, Int_t b, TString z = "clockwise") //calculating square error of correlation coefficient
+    {
+        Int_t N_parallel = 0;
+        Int_t N_perpendicular = 0;
+        Int_t N_mixed_1 = 0;
+        Int_t N_mixed_2 = 0;
+        local_count_coincidences(a,b,NumEvents, N_parallel, N_perpendicular, N_mixed_1, N_mixed_2);
         return 
         (float)4.0/pow((N_parallel+N_perpendicular+N_mixed_1+N_mixed_2),4)*(pow(N_mixed_1+N_mixed_2,2)*(N_parallel+N_perpendicular)+pow(N_parallel+N_perpendicular,2)*(N_mixed_1+N_mixed_2))
-
     ;
     }
 
-
-    Float_t calculate_local_CHSH(Int_t NumEvents[16][16], Int_t a_angle, Int_t angle_between_counters)
+    Float_t calculate_local_CHSH(Int_t NumEvents[16][16], Int_t a_angle, Int_t angle_between_counters, TString z = "clockwise")
     {
+        if (z == "counterclockwise") angle_between_counters = -angle_between_counters;
         return
-        E_coeff(NumEvents,a_angle,a_angle+angle_between_counters) -
-        E_coeff(NumEvents,a_angle,a_angle+3*angle_between_counters) +
-        E_coeff(NumEvents,a_angle+2*angle_between_counters,a_angle+angle_between_counters) +
-        E_coeff(NumEvents,a_angle+2*angle_between_counters,a_angle+3*angle_between_counters);
+        E_coeff(NumEvents,a_angle,a_angle+angle_between_counters,z) -
+        E_coeff(NumEvents,a_angle,a_angle+3*angle_between_counters,z) +
+        E_coeff(NumEvents,a_angle+2*angle_between_counters,a_angle+angle_between_counters,z) +
+        E_coeff(NumEvents,a_angle+2*angle_between_counters,a_angle+3*angle_between_counters,z);
     }
 
-    Float_t calculate_local_CHSH_error(Int_t NumEvents[16][16], Int_t a_angle, Int_t angle_between_counters)
+    Float_t calculate_local_CHSH_error(Int_t NumEvents[16][16], Int_t a_angle, Int_t angle_between_counters, TString z = "clockwise")
     {
+        if (z == "counterclockwise") angle_between_counters = -angle_between_counters;
         return
-        sqr_E_error(NumEvents,a_angle,a_angle+angle_between_counters) +
-        sqr_E_error(NumEvents,a_angle,a_angle+3*angle_between_counters) +
-        sqr_E_error(NumEvents,a_angle+2*angle_between_counters,a_angle+angle_between_counters) +
-        sqr_E_error(NumEvents,a_angle+2*angle_between_counters,a_angle+3*angle_between_counters);
-
+        sqr_E_error(NumEvents,a_angle,a_angle+angle_between_counters,z) +
+        sqr_E_error(NumEvents,a_angle,a_angle+3*angle_between_counters,z) +
+        sqr_E_error(NumEvents,a_angle+2*angle_between_counters,a_angle+angle_between_counters,z) +
+        sqr_E_error(NumEvents,a_angle+2*angle_between_counters,a_angle+3*angle_between_counters,z);
     }
 }
 #endif
