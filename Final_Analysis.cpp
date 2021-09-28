acbfa8024a16b694c1e7317d60ce7c62fcf58e42
Alexander <deceptimator@yandex.ru>
2021-09-28 13:05:39 +0300

    Pushed functions into separate namespace

--- Final_Analysis.cpp
+++ Final_Analysis.cpp
@@ -21,145 +21,28 @@
 #include <TObjString.h>
 #include "../calo_analysis/Readme_reader.h"
 #include "ChannelEntry.h"
+#include "CHSH_calculator.h"
+
 using namespace std;
+using namespace CHSH;
 
 #define UseManyRoots 0
 #define PresentDiffuser 1
 #define CalculateRatio 1
 #define DrawTime 1
 #define UseIntegralCut 1
-#define UseNotEntangledPhotons 1
+#define UseNotEntangledPhotons 0
 #define TotalEnergyCut 0
 #define DrawToPDF 0
 #define calculate_CHSH 1
-/////////////////////////////////////////////////////
-#if calculate_CHSH
-
-void count_coincidences(Int_t n, Int_t NumEvents[16][16], Int_t &N_parallel, Int_t &N_perpendicular, Int_t &N_mixed_1, Int_t &N_mixed_2)
-{
-        N_parallel=0;
-        N_perpendicular = 0;
-        N_mixed_1 = 0;
-        N_mixed_2 = 0;  
-
-    for (Int_t a = 0; a < 16; a++)
-    {
-        //if (string == "clockwise") 
-        Int_t b = a+n;
-        //if (string == "counterclockwise") Int_t b = a-n+16;
-        Int_t a_1 = a+4;
-        Int_t b_1 = b+4;
-        while (a > 15) a=a-16;
-        while (b > 15) b=b-16;
-
-        while (a_1> 15) a_1=a_1-16;
-        while (b_1 > 15) b_1=b_1-16;    
-        N_parallel += NumEvents[a][b];
-        N_perpendicular += NumEvents[a_1][b_1];
-        N_mixed_1 += NumEvents[a][b_1];
-        N_mixed_2 += NumEvents[a_1][b];        
-    }
-}
-
-Float_t global_E_coeff(Int_t n, Int_t NumEvents[16][16])
-{
-    Int_t N_parallel = 0; Int_t N_perpendicular = 0;Int_t N_mixed_1 = 0;Int_t N_mixed_2 = 0;
-    count_coincidences(n,NumEvents, N_parallel, N_perpendicular, N_mixed_1, N_mixed_2);
-    return (float)(N_parallel+N_perpendicular-N_mixed_1-N_mixed_2)/(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2)
-    ;
-}
-Float_t global_sqr_E_error(Int_t n,Int_t NumEvents[16][16]) //calculating square error of correlation coefficient
-{
-    Int_t N_parallel = 0; Int_t N_perpendicular = 0;Int_t N_mixed_1 = 0;Int_t N_mixed_2 = 0;
-
-    count_coincidences(n,NumEvents, N_parallel, N_perpendicular, N_mixed_1, N_mixed_2);
-    //return pow(*(N_mixed_1+N_mixed_2)/pow((N_parallel+N_perpendicular+N_mixed_1+N_mix)ed_2),2),2)*(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2);
-    return (float)4.0/pow((N_parallel+N_perpendicular+N_mixed_1+N_mixed_2),4)*(pow(N_mixed_1+N_mixed_2,2)*(N_parallel+N_perpendicular)+pow(N_parallel+N_perpendicular,2)*(N_mixed_1+N_mixed_2));
-}
-Float_t global_calculate_CHSH(Int_t n,Int_t NumEvents[16][16])
-{
-
-    return
-    3*global_E_coeff(n, NumEvents) - global_E_coeff(3*n,NumEvents)
-    ;
-}
-
-Float_t global_calculate_CHSH_error(Int_t n,Int_t NumEvents[16][16])
-{
-    return
-    global_sqr_E_error(n, NumEvents)*3 + global_sqr_E_error(3*n, NumEvents)
-    ;
-}
-
-
-
-Float_t E_coeff(Int_t NumEvents[16][16], Int_t a, Int_t b)
-{
-    Int_t a_1 = a+4;
-    Int_t b_1 = b+4;
-    while (a > 15) a=a-16;
-    while (b > 15) b=b-16;
-
-    while (a_1> 15) a_1=a_1-16;
-    while (b_1 > 15) b_1=b_1-16;    
-    Int_t N_parallel=NumEvents[a][b];
-    Int_t N_perpendicular = NumEvents[a_1][b_1];
-    Int_t N_mixed_1 = NumEvents[a][b_1];
-    Int_t N_mixed_2 = NumEvents[a_1][b];
-    return (float)(N_parallel+N_perpendicular-N_mixed_1-N_mixed_2)/(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2)
-    ;
-}
-Float_t sqr_E_error(Int_t NumEvents[16][16], Int_t a, Int_t b) //calculating square error of correlation coefficient
-{
-    Int_t a_1 = a+4;
-    Int_t b_1 = b+4;
-    while (a > 15) a=a-16;
-    while (b > 15) b=b-16;
-
-    while (a_1> 15) a_1=a_1-16;
-    while (b_1 > 15) b_1=b_1-16;    
-    Int_t N_parallel=NumEvents[a][b];
-    Int_t N_perpendicular = NumEvents[a_1][b_1];
-    Int_t N_mixed_1 = NumEvents[a][b_1];
-    Int_t N_mixed_2 = NumEvents[a_1][b];
-    //return pow(2*(N_mixed_1+N_mixed_2)/pow((N_parallel+N_perpendicular+N_mixed_1+N_mix)ed_2),2),2)*(N_parallel+N_perpendicular+N_mixed_1+N_mixed_2);
-    return 
-    (float)4.0/pow((N_parallel+N_perpendicular+N_mixed_1+N_mixed_2),4)*(pow(N_mixed_1+N_mixed_2,2)*(N_parallel+N_perpendicular)+pow(N_parallel+N_perpendicular,2)*(N_mixed_1+N_mixed_2))
-
-;
-}
-
-
-Float_t calculate_local_CHSH(Int_t NumEvents[16][16], Int_t a_angle, Int_t angle_between_counters)
-{
-    return
-    E_coeff(NumEvents,a_angle,a_angle+angle_between_counters) -
-    E_coeff(NumEvents,a_angle,a_angle+3*angle_between_counters) +
-    E_coeff(NumEvents,a_angle+2*angle_between_counters,a_angle+angle_between_counters) +
-    E_coeff(NumEvents,a_angle+2*angle_between_counters,a_angle+3*angle_between_counters);
-}
-
-Float_t calculate_local_CHSH_error(Int_t NumEvents[16][16], Int_t a_angle, Int_t angle_between_counters)
-{
-    return
-    sqr_E_error(NumEvents,a_angle,a_angle+angle_between_counters) +
-    sqr_E_error(NumEvents,a_angle,a_angle+3*angle_between_counters) +
-    sqr_E_error(NumEvents,a_angle+2*angle_between_counters,a_angle+angle_between_counters) +
-    sqr_E_error(NumEvents,a_angle+2*angle_between_counters,a_angle+3*angle_between_counters);
-
-    
-}
-
-
-
-#endif
 
 /////////////////////////////////////////////////////
 void Final_Analysis()
 {
+    const Int_t events_divider = 100;
 	gStyle->SetOptFit(1);
 	//gStyle->SetOptStat(1111);
-	TString source_path = "/home/doc/entanglement/root_files_data/collimator_4_8_cm_further_from_diffuser/";
+	TString source_path = "/home/doc/entanglement/root_files_data/with_scatterer/big_file/";
 #if UseNotEntangledPhotons
     TString result_path  = source_path + "result_ev_b_ev_decoh_new";
 #else
@@ -266,7 +149,7 @@ void Final_Analysis()
 ///////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\////////////////////////
     const Int_t calculate_Events_Number =  
     
-    PMT_tree->GetEntries()/1
+    PMT_tree->GetEntries()/events_divider
     ;
     const Int_t events_wanted = 10000000;
     Int_t Events_for_cuts = 0;
